781eb0569a6ac6b34131eb09c0085ba711f869ae
Slavi15 <slavirezashkiofficial@gmail.com>
2021-03-07 22:46:00 +0200

    ADD analyze and flip board, FETCH data from lichess API and UPDATE styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 let board = null;
 let chess = new Chess();
-import chessEcoCodes from 'https://cdn.skypack.dev/pin/chess-eco-codes@v0.0.0-SmTuNwaJJtW3pMk2tcVz/mode=imports,min/optimized/chess-eco-codes.js';
 let $status = $('#status');
 let $fen = $('#fen');
 let $pgn = $('#pgn');
 let $com = $('#com');
+let $exp = $('.title3');
+let $explorer = $('#explorer');
 
 function onDragStart(source, piece, position, orientation) {
     if(chess.game_over()) {
@@ -32,21 +33,73 @@ function onDrop(source, target) {
 
 function onSnapEnd () {
     board.position(chess.fen());
-    let openingName = chessEcoCodes(chess.fen());
-    if(openingName !== undefined) {
-        $com.html(openingName.name);
-    } else if(openingName == undefined) {
-        return false;
-    }
-    //console.log(openingName)
 };
 
 function onChange(oldPos, newPos) {
     console.log('Old position: ' + Chessboard.objToFen(oldPos))
     console.log('New position: ' + Chessboard.objToFen(newPos))
 
-    console.log(chess.history());
-}
+    let url = `https://explorer.lichess.ovh/lichess?variant=standard&moves=6&speeds[]=rapid&speeds[]=classical&ratings[]=1600&ratings[]=1800&fen=${chess.fen()}`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let openingData = Object.values(data)[5];
+            let playerData = Object.values(data)[3];
+            //console.dir(playerData);
+
+            $explorer.html(`
+                <div class="move">${playerData[0].san}</div>
+                <div class="number">${playerData[0].white + playerData[0].draws + playerData[0].black}</div>
+                <div class="wins">
+                    <div class="white">${playerData[0].white}</div>
+                    <div class="draw">${playerData[0].draws}</div>
+                    <div class="black">${playerData[0].black}</div>
+                </div>
+
+                <div class="move">${playerData[1].san}</div>
+                <div class="number">${playerData[1].white + playerData[1].draws + playerData[1].black}</div>
+                <div class="wins">
+                    <div class="white">${playerData[1].white}</div>
+                    <div class="draw">${playerData[1].draws}</div>
+                    <div class="black">${playerData[1].black}</div>
+                </div>
+
+                <div class="move">${playerData[2].san}</div>
+                <div class="number">${playerData[2].white + playerData[2].draws + playerData[2].black}</div>
+                <div class="wins">
+                    <div class="white">${playerData[2].white}</div>
+                    <div class="draw">${playerData[2].draws}</div>
+                    <div class="black">${playerData[2].black}</div>
+                </div>
+
+                <div class="move">${playerData[3].san}</div>
+                <div class="number">${playerData[3].white + playerData[3].draws + playerData[3].black}</div>
+                <div class="wins">
+                    <div class="white">${playerData[3].white}</div>
+                    <div class="draw">${playerData[3].draws}</div>
+                    <div class="black">${playerData[3].black}</div>
+                </div>
+
+                <div class="move">${playerData[4].san}</div>
+                <div class="number">${playerData[4].white + playerData[4].draws + playerData[4].black}</div>
+                <div class="wins">
+                    <div class="white">${playerData[4].white}</div>
+                    <div class="draw">${playerData[4].draws}</div>
+                    <div class="black">${playerData[4].black}</div>
+                </div>
+            `);
+
+            if(openingData !== null) {
+                $com.html(openingData.name);
+                $exp.html(`<b>${openingData.eco}</b> ${openingData.name}`) 
+            } else if(openingData == null) {
+                return false
+            }      
+        });
+
+    //console.log(chess.history());
+};
 
 $('#prevBtn').on('click', () => {
     chess.undo();
@@ -62,6 +115,23 @@ $('#nextBtn').on('click', () => {
 
 $('#resetBtn').on('click', () => {
     window.location.reload();
+});
+
+$('#flipBtn').on('click', () => {
+    board.flip();
+});
+
+$('#analyzeBtn').on('click', () => {
+    const stats3 = document.getElementById('stats3');
+    const inner3 = document.getElementById('inner3');
+
+    if(stats3.style.display == 'none') {
+        stats3.style.display = 'block';
+        inner3.style.display = 'block';
+    } else {
+        stats3.style.display = 'none';
+        inner3.style.display = 'none';
+    }
 });
 
 function updateStatus() {
@@ -92,6 +162,7 @@ function updateStatus() {
 
 if(chess.fen() == 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1') {
     $com.html('Play first move!');
+    $exp.html('No opening played!')
 };
 
 let config = {
